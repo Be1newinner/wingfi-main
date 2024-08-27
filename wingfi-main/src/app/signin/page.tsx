91ab46f3c66214508587b0f22b8cbccd1cd621d2
Vijay Kumar <Be1newinner@gmail.com>
2024-08-27 10:56:58 +0530

    added Suspense around sign in page

--- wingfi-main/src/app/signin/page.tsx
+++ wingfi-main/src/app/signin/page.tsx
@@ -1,10 +1,13 @@
 import SingInForms from "./SignInForm";
 import { SignInWrapper } from "../../components";
+import { Suspense } from "react";
 
 export default function SignIn() {
   return (
     <SignInWrapper>
-      <SingInForms />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SingInForms />
+      </Suspense>
     </SignInWrapper>
   );
 }
