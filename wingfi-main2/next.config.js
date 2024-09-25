a520a82919fec75fe4f1592f364e9f7f3d6acb7c
Vijay Kumar <Be1newinner@gmail.com>
2024-09-25 22:52:42 +0530

    new wingfi test

--- wingfi-main2/next.config.js
+++ wingfi-main2/next.config.js
@@ -17,15 +17,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        'undici': false,
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
