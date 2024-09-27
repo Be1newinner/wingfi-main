071bd82991b8ec86aad8995c03af084aacce2b64
Vijay Kumar <Be1newinner@gmail.com>
2024-09-28 00:01:13 +0530

    allow all

--- wingfi-admin/config/cors.js
+++ wingfi-admin/config/cors.js
@@ -2,7 +2,7 @@ import Cors from "cors";
 
 // Initialize the cors middleware
 const cors = Cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
+    origin: "*", // Allow requests from this origin
     methods: ["GET", "POST"],        // Allowed methods
 });
 
