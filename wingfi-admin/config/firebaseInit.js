90e2e6f79f74ab8c3425549c2792c627ae3c65b0
Vijay Kumar <Be1newinner@gmail.com>
2024-10-06 11:15:16 +0530

    now commit

--- wingfi-admin/config/firebaseInit.js
+++ wingfi-admin/config/firebaseInit.js
@@ -1,10 +1,12 @@
 import admin from "firebase-admin";
+
 let privateKey = "";
+
 if (process.env.FIREBASE_PRIVATE_KEY) {
   privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
 }
 
-const app = admin.initializeApp({
+export const app = admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     privateKey,
@@ -14,8 +16,6 @@ const app = admin.initializeApp({
     "https://wingfi-9b5b7-default-rtdb.asia-southeast1.firebasedatabase.app",
 });
 
-const firestore = admin.firestore(app);
-const auth = admin.auth(app);
-const realtimeDb = admin.database(app);
-
-module.exports = { app, firestore, auth, realtimeDb };
+export const firestore = admin.firestore(app);
+export const auth = admin.auth(app);
+export const realtimeDb = admin.database(app);
