7743b9f2e8cac222bae483eb8089d15500645498
arsh14082002 <thesiddiqui7@gmail.com>
2024-10-09 12:34:10 +0530

    add and favourite button

--- wingfi-main/src/components/ProductViewSm/ProductViewList3.jsx
+++ wingfi-main/src/components/ProductViewSm/ProductViewList3.jsx
@@ -1,8 +1,14 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaStar } from 'react-icons/fa';
+import { FaRegHeart, FaHeart } from 'react-icons/fa'; // Import filled heart icon
+
+import { useState } from 'react';
 
 export function ProductViewList3({ item, rating }) {
+  const [isFavorite, setIsFavorite] = useState(false); // State to manage heart icon
+
   const titleFormat = {
     display: '-webkit-box',
     WebkitBoxOrient: 'vertical',
@@ -12,7 +18,7 @@ export function ProductViewList3({ item, rating }) {
   };
 
   return (
-    <Link className="shadow-md border p-5 w-full" href={'/shop/' + item?.slug}>
+    <li className="relative shadow-md border p-5 w-full">
       <div className="overflow-hidden transition-transform duration-300 hover:scale-110">
         <Image
           src={`https://firebasestorage.googleapis.com/v0/b/wingfi-9b5b7.appspot.com/o/pro%2F${item.sku}%2F0.webp?alt=media`}
@@ -22,12 +28,24 @@ export function ProductViewList3({ item, rating }) {
           className="h-[150px] w-full object-contain"
         />
       </div>
-      <div className="flex flex-col gap-2 mt-3">
-        <h2 className="leading-6 font-bold text-lg" style={titleFormat}>
-          {item?.title}
-        </h2>
 
-        <div className="flex items-end justify-between">
+      <span
+        className={`absolute top-[20px] right-[20px] transition-transform duration-300 ${
+          isFavorite ? 'scale-100 text-red-500' : 'scale-110 text-gray-400'
+        }`}
+        onClick={() => setIsFavorite(!isFavorite)} // Toggle favorite state
+      >
+        {isFavorite ? <FaHeart /> : <FaRegHeart />} {/* Conditional rendering of heart */}
+      </span>
+
+      <div className="flex flex-col gap-2 mt-3">
+        <Link href={'/shop/' + item?.slug}>
+          <h2 className="leading-6 font-bold text-lg" style={titleFormat}>
+            {item?.title}
+          </h2>
+        </Link>
+
+        <div className="flex items-end justify-between cursor-pointer">
           <div>
             <div className="flex gap-1 text-yellow-400">
               {Array.from({ length: 5 }, (_, index) => (
@@ -45,12 +63,12 @@ export function ProductViewList3({ item, rating }) {
           </div>
 
           <div>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded text-md font-bold">
+            <button className="bg-red-500 text-white px-4 py-2 rounded text-md font-bold">
               Add to Cart
             </button>
           </div>
         </div>
       </div>
-    </Link>
+    </li>
   );
 }
